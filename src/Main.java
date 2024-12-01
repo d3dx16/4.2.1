b898e74faa5652a3ae04376e3ade41b1b7b79117
Yuriy.Boykov <d3dx16@gmail.com>
2024-12-01 13:29:13 +0700

    Изменения 13:28 1.12.2024

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         String text = "Regular Expressions or Regex is an API for defining String patterns that can be used for searching, manipulating and editing a text. It is widely used to define a constraint on strings such as a password. Regular Expressions are provided under java.util.regex package.";
-        Pattern pattern = Pattern.compile("\\bst.{3}s");//\b(st)(.{3,5})(s)
+        Pattern pattern = Pattern.compile("\\bst.{4}s");//\b(st)(.{3,5})(s)
 
         Matcher matcher = pattern.matcher(text);// где ищем
 
